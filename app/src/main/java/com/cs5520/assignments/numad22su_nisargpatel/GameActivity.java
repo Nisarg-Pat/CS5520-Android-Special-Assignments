5b6a73603e6c43141bf2756ad24d70b6966e3518
Nisarg Patel <patel321nisarg@gmail.com>
2022-05-29 02:16:49 -0400

    Week-2: GameActivity opening to full screen

--- app/src/main/java/com/cs5520/assignments/numad22su_nisargpatel/GameActivity.java
+++ app/src/main/java/com/cs5520/assignments/numad22su_nisargpatel/GameActivity.java
@@ -3,12 +3,14 @@ package com.cs5520.assignments.numad22su_nisargpatel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game);
     }
 }
