c9e29f471267fbe77eb9be3e7d3267588d30f604
Nisarg Patel <patel321nisarg@gmail.com>
2022-05-29 03:38:48 -0400

    Week-2: GameActivity Upgrade Button onclick text change implemented.

--- app/src/main/java/com/cs5520/assignments/numad22su_nisargpatel/GameActivity.java
+++ app/src/main/java/com/cs5520/assignments/numad22su_nisargpatel/GameActivity.java
@@ -3,14 +3,34 @@ package com.cs5520.assignments.numad22su_nisargpatel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
+
+    Button upgradeBuy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game);
+
+        upgradeBuy = findViewById(R.id.upgrade_button);
+        upgradeBuy.setOnClickListener((v) -> changeUpgradeBuy());
+    }
+
+    private void changeUpgradeBuy() {
+        String s = upgradeBuy.getText().toString();
+        if (s.equals(getString(R.string.upgrade_buy_x1))) {
+            upgradeBuy.setText(R.string.upgrade_buy_x10);
+        } else if (s.equals(getString(R.string.upgrade_buy_x10))) {
+            upgradeBuy.setText(R.string.upgrade_buy_x100);
+        } else if (s.equals(getString(R.string.upgrade_buy_x100))) {
+            upgradeBuy.setText(R.string.upgrade_buy_next);
+        } else {
+            upgradeBuy.setText(R.string.upgrade_buy_x1);
+        }
     }
 }
