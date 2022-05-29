f11aa2f34146a1a2a030f5afc21f48d64b64210d
Nisarg Patel <patel321nisarg@gmail.com>
2022-05-29 02:05:19 -0400

    Week-2: Added New empty activity: GameActivity for AdCap Clone

--- app/src/main/java/com/cs5520/assignments/numad22su_nisargpatel/MainActivity.java
+++ app/src/main/java/com/cs5520/assignments/numad22su_nisargpatel/MainActivity.java
@@ -2,6 +2,7 @@ package com.cs5520.assignments.numad22su_nisargpatel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button aboutMeBtn;
+    Button aboutMeBtn,adCapCloneBtn;
     TextView helloWorldTV;
 
     @Override
@@ -18,15 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         aboutMeBtn = findViewById(R.id.about_me_btn);
-        aboutMeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showToast();
-            }
-        });
+        aboutMeBtn.setOnClickListener((v) -> showToast());
+        adCapCloneBtn = findViewById(R.id.adcap_clone_btn);
+        adCapCloneBtn.setOnClickListener((v) -> openGame());
     }
 
     private void showToast() {
         Toast.makeText(this, "Name: Nisarg Patel\nEmail ID: patel.nisargs@northeastern.edu", Toast.LENGTH_LONG).show();
     }
+
+    private void openGame() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
 }
